0fe5fd0deb03ce871f535362c24c7761a942b15c
sangbate258 <sangbate258@gmail.com>
2025-08-27 12:37:15 +0700

    Done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Promise-JS
-
+* Promise sinh ra là để giải quyết vấn đề callback hell, xử lý bất đồng bộ
+* Khởi tạo bằng từ khóa new. Đối số của promise là một function. Function này sẽ bắt buộc trả về 2 tham số (2 tham số này đều là hàm) là resolve(nếu thành công) và reject(nếu thất bại)
+* Để sử dụng ta dùng .then(hàm thực hiện công việc cho resolve), .catch(hàm thực hiện cho reject)
---
+++ main.js
@@ -0,0 +1,12 @@
+//test cách chạy của Promise
+// const promise = new Promise((resolve, reject)=>{
+//     reject([1,2,3]);
+// });
+// promise.then(() => console.log("thành công")).catch((array)=>console.log(array)).finally(()=>console.log("xong!"));
+
+//promise nấu cơm
+// const cook = new Promise((cookSuccess, cookFailure)=>{
+//     let resultCook = false;
+//     resultCook?(cookSuccess(`cơm chín rồi ăn thoai`)): cookFailure('cơm khét rồi :(');
+// });
+// cook.then(element => console.log(element)).catch(element=>console.log(element)).finally(()=>console.log(`kết thúc trận chiến`));
