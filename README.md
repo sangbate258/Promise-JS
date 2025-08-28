30f391710881b617d86d68907d5fa7f91e7ec451
sangbate258 <sangbate258@gmail.com>
2025-08-28 15:09:09 +0700

    Done

--- README.md
+++ README.md
@@ -4,8 +4,15 @@
 - Khởi tạo bằng từ khóa new. Đối số của promise là một function. Function này sẽ bắt buộc trả về 2 tham số (2 tham số này đều là hàm) là resolve(nếu thành công) và reject(nếu thất bại)
 - Để sử dụng ta dùng .then(hàm thực hiện công việc cho resolve), .catch(hàm thực hiện cho reject)
 
-## Tìm hiểu sau về promise
-
 - Promise cũng giống callback hell đó là **__chờ thằng trước làm xong rồi mới đến thằng sau__** nhưng chỉ khác ở chỗ là cách viết nó ngắn gọn, dễ đọc hơn thôi
 - Có thể có nhiều .then. Giá trị được return ở .then đầu tiên sẽ là giá trị cho tham số đầu vào của .then thứ 2 và cứ thế tiếp tục
--
+- Promise đưa ra (return) một giá trị nằm trong resolve hoặc reject còn .then sẽ nhận vào giá trị đó và cần một **return** để đưa cho .then sau
+- Cách để đưa promise ở trạng thái luôn luôn thành công hoặc luôn luôn thất bại là dùng: Promise.resolve() hoặc Promise.reject() Ví dụ: const promise = Promise.resolve("result");
+- Promise.all giúp chạy song song các promise. Nó nhận vào một mảng các promise và trả về 1 promise với resolve hoặc reject
+- Promise.all chỉ resolve khi tất cả mảng đều resolve nếu 1 trong các mảng reject thì nó cũng reject
+
+## async/await
+- Là hiện thân của promise nhưng viết dễ hiểu hơn theo phong cách đồng bộ vậy nên luôn phải try(resolve) catch(reject) 
+- async viết trước 1 function thì khi return nó sẽ trả về một promise
+- await là chờ cho hàm xong rồi (resolve/reject) mới chạy tiếp dòng bên dưới
+- await chỉ được dùng bên trong async
--- main.js
+++ main.js
@@ -13,24 +13,113 @@
 
 //Cách hoạt động để giải quyết callback hell
 //Sau 1s xuất ra 1, 2, 3
-let data = 1;
-function sleep(data, ms)
-{
-    return new Promise((resolve, reject) =>{
-        setTimeout(() => {
-            console.log(data);
-            resolve(data+1);//2
-        }, ms);
-    })
-}
-sleep(data, 1000)
-    .then((data) =>{
-        sleep(data, 1000);
-    })
-    .then((data)=>{
-        sleep(data, 1000);
-    })
-    .then(()=>{
-        console.log("Kết thúc!");
-    })
-    
+// let data = 1;
+// function sleep(data,ms)
+// {
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             console.log(data);
+//             resolve(data+1);
+//         }, ms);
+//     })
+// }
+
+// sleep(data,1000)
+//     .then((data)=>{
+//         return sleep(data,1000)
+//     })
+//     .then((data)=>{
+//         return sleep(data,1000);
+//     })
+
+//Promise.resolve() và promise.reject()
+// const promise = Promise.resolve("luôn luôn success");
+// promise.then((element)=>{
+//     console.log(element);
+// })
+
+//Promise.all
+// const p1 = new Promise((resolve)=>{
+//     setTimeout(() => {
+//         resolve([1]);
+//     }, 1000);
+// });
+
+// const p2 = new Promise((resolve)=>{
+//     setTimeout(() => {
+//         resolve([2,3]);
+//     }, 3000);
+// });
+// Promise.all([p1,p2]).then(([result1, result2])=>{//destructuring
+//     console.log(result1.concat(result2));
+// })
+
+//vi du 1 cho Promise.all khong bi reject
+// const p1 = Promise.resolve("A");
+// const p2 = Promise.resolve("B");
+// const p3 = Promise.resolve("C");
+// Promise.all([p1,p2,p3]).then((result)=>{
+//     console.log(result);
+// })
+//Ví dụ 2 cho Promise.all bị reject
+// const p4 = Promise.reject("có lỗi rồi");
+// Promise.all([p1,p4,p3]).then((result)=>{
+//     console.log(result);
+// })
+// .catch((result)=>{
+//     console.log(result);
+// });
+
+//async/await
+//Ví dụ 1: Nối mảng (chạy song song)
+// const p1 = new Promise((resovle)=>{
+//     setTimeout(() => {
+//         resovle([1])
+//     }, 1000);
+// })
+// const p2 = new Promise((resovle)=>{
+//     setTimeout(() => {
+//         resovle([2, 3])
+//     }, 3000);
+// })
+// async function test() {
+//     try{
+//         const [a,b] = await Promise.all([p1,p2])
+//         console.log(a.concat(b));
+//     }catch{ 
+//         console.log("sai")
+//     };
+// }
+// test()
+
+//Ví dụ 2: cứ 1 giây in ra 1, 2, 3
+
+// function sleep(data, ms)
+// {
+//     return new Promise((resolve) =>{
+//         setTimeout(() => {
+//             console.log(data);
+//             resolve(data+1)
+//         }, ms);
+//     })
+// }
+// async function test(data, ms)
+// {
+//     try {
+//         for(let i = 0; i < 10; i++)
+//         {
+//             data = await sleep(data,ms);   
+//         }
+//     } catch{
+//         console.log("Lỗi rồi");
+//     }
+
+//     // try{
+//     // const first = await sleep(data, ms);
+//     // const second = await sleep(first,ms);
+//     // const third = await sleep(second,ms);
+//     // }catch{
+//     //     console.log("Lỗi rồi");
+//     // }
+// }
+// test(1,1000);
